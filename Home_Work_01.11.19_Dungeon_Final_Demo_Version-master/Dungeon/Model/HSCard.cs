26541fe8c1408757072302be8674de1247e7b666
Traumtanzen <55258207+Traumtanzen@users.noreply.github.com>
2019-12-04 17:43:23 +0300

    Add files via upload

---
+++ Home_Work_01.11.19_Dungeon_Final_Demo_Version-master/Dungeon/Model/HSCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dungeon.Actions;
+
+namespace Dungeon.Model
+{
+    public class HSCard
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public HSSpecificHandler Handler { get; set; }
+    }
+}
---
+++ Home_Work_01.11.19_Dungeon_Final_Demo_Version-master/Dungeon/Model/HSSpecificHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon.Model
+{
+    public class HSSpecificHandler
+    {
+        public HandlerType HandlerType { get; set; }
+        public string Description { get; set; }
+        public int Damage { get; set; }
+    }
+
+    public enum HandlerType
+    {
+        None,
+        BattleCry,
+        DeathRattle
+    }
+}

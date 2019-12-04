eefb69531fd7f781303672eba9be42eb9888ca9c
Traumtanzen <55258207+Traumtanzen@users.noreply.github.com>
2019-12-04 14:54:18 +0300

    Add files via upload
    Added DurakCard class

---
+++ Home_Work_01.11.19_Dungeon_Final_Demo_Version-master/Dungeon/Model/DurakCard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dungeon.Model
+{
+    public class DurakCard
+    {
+        public Suits Suit { get; set; }
+        public Faces Face { get; set; }
+
+        public string ShowCard()
+        {
+            return $"{Face} of {Suit}";
+        }
+    }
+    public enum Suits
+    {
+        Hearts,
+        Diamonds,
+        Spades,
+        Clubs
+    }
+    public enum Faces
+    {
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+        Ace = 14,
+    }
+}
+
